4d50f1f71a7ddf1e00d59619850ef02c0a69fa76
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 19:36:17 +0530

    adding changes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Accordion from './Accordion'
+const texts = [
+  {
+    title: 'Arsenal',
+    subTitle: 'Arsenal Football Club is a professional football club based in Islington, London, England that plays in the Premier League, the top flight of English football.'
+  },
+  {
+    title: 'Manchester United',
+    subTitle: 'Manchester United Football Club is a professional football club based in Old Trafford, Greater Manchester, England, that competes in the Premier League, the top flight of English football. '
+  },
+  {
+    title: 'Manchester City',
+    subTitle: 'Manchester City Football Club is an English football club based in Manchester that competes in the Premier League, the top flight of English football.'
+  }
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion texts = {texts}></Accordion>
     </div>
   );
 }
+
+
 
 export default App;
