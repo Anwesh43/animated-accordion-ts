e6cbe763ed743c17c9408346d57ca550fa06304e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 19:35:30 +0530

    adding fixes in AccordionItem

--- src/AccordionItem.tsx
+++ src/AccordionItem.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import {useStyle} from './hooks'
 
+
+interface AccordionText {
+    title : string, 
+    subTitle: string
+}
+
 interface ItemProps {
-    text : String, 
+    text : AccordionText, 
     onClick: Function, 
     currI: number, 
     prevI: number,
     scale : number,
     w : number,
-    h : number  
+    h : number ,
+    i : number
 }
 
-const AccordionItem = (props : ItemProps) => {
+const AccordionItem : React.FC<ItemProps> = (props : ItemProps) => {
     const {headerStyle, itemStyle} = useStyle(props.w, props.h, props.scale)
+    console.log("Scale",props.scale)
     return (
-        <div onClick = {() => props.onClick()}>
-            <div style = {headerStyle()}></div>
+        <div onClick = {() => props.onClick(props.i)}>
+            {}
+            <div style = {headerStyle()}>{props.text.title}</div>
+            <div style = {itemStyle()}>{props.text.subTitle}</div>
         </div>
     )
 }
