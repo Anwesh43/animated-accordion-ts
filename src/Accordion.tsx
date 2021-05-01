abd1390bf324f4fa966db9dec942bfdd44b69922
Anwesh43 <anweshthecool0@gmail.com>
2021-05-01 20:20:04 +0530

    adding new features

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useAnimatedScale, useDimension} from './hooks'
+import {useAnimatedScale, useDimension, useStyle} from './hooks'
 import AccordionItem from './AccordionItem'
 
 interface AccordionText {
@@ -9,18 +9,13 @@ interface AccordionText {
 interface AccordionProps {
     texts : Array<AccordionText> 
 } 
-const accordionStyle = () => (
-    {
-        display: 'flex',
-        justifyContent: 'space-around'
-    }
-)
 const getScale = (i : number, scale : number, currI : number, prevI : number) => (
     i === currI ? scale : (prevI == i) ? (1 - scale) : 0 
 )
 const Accordion : React.FC<AccordionProps> = (props : AccordionProps) => {
     const {w, h} = useDimension()
     const {scale, prevI, currI, start} = useAnimatedScale()
+    const {accordionStyle} = useStyle(w, h, scale)
     return <div>
         {props.texts.map((text : AccordionText, i : number) => (<AccordionItem i = {i} text = {text} w = {w} h = {h} scale = {getScale(i, scale, currI, prevI)} currI = {currI} prevI = {prevI} onClick = {start}></AccordionItem>))}
     </div>
--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,7 @@ import {useState, useEffect, CSSProperties} from 'react'
 
 const delay : number = 20 
 const scGap : number = 0.1 
+const hFactor : number = 5 
 export const useAnimatedScale = () => {
     const [currI, setCurrI] : [number, Function]  = useState(-1)
     const [prevI, setPrevI] : [number, Function] = useState(-1)
@@ -58,27 +59,39 @@ export const useDimension = () => {
 }
 
 export const useStyle = (w : number, h : number, scale : number) => {
-    const hFactor : number = h / 6
+    const barH : number = h / hFactor
     return {
         itemStyle() : CSSProperties {
+            const boxShadow : string | undefined= scale == 0 ? undefined : '5px 10px  10px 5px rgba(13,71,161 ,0.4)'
             return {
-                width : `${w / 3}px`,
-                height : `${hFactor * scale}px`,
-                background : '#2962FF',
+                width : `${w * 0.8}px`,
+                height : `${barH * scale}px`,
+                marginLeft: `${w * 0.1}px`,
+                background : '#0D47A1',
                 fontSize : `20px`,
                 color : 'white', 
-                textAlign: 'center',
-                overflow: 'hidden'
+                overflow: 'hidden',
+                boxShadow
             }
         },
         headerStyle() : CSSProperties {
             return {
-                width : `${w / 3}px`,
+                marginLeft: `${w * 0.1}px`,
+                width : `${w * 0.8}px`,
                 height : `${h / 15}px`,
                 fontSize : `30px`,
                 color: 'white',
-                background: '#2962FF'
+                background: 'rgba(13,71,161 ,0.9)',
+                marginTop: `${h / 30}px`,
+                cursor: 'pointer'
             }
         },
+        accordionStyle(n : number) : CSSProperties {
+            return {
+                height: `${1.6 * (barH) * n}px`,
+                display: 'flex',
+                justifyContent: 'space-around'
+            }
+        }
     }
 }
